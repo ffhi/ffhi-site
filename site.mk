a6d553b53fbb6ffa16ec3db4348ee5106700230b
Markus Pargmann <mpa@devmp.org>
2015-04-14 22:17:13 +0200

    0.6.1 release
    Signed-off-by: Markus Pargmann <mpa@devmp.org>

--- site.mk
+++ site.mk
@@ -33,7 +33,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.6
+DEFAULT_GLUON_RELEASE := 0.6.1
 
 
 ##	GLUON_RELEASE

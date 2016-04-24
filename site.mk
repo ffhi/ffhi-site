364d41facd7d73048e55d770aaf79fcbd70914fa
Markus Pargmann <mpa@devmp.org>
2016-04-24 17:24:30 +0200

    update site.mk
    Signed-off-by: Markus Pargmann <mpa@devmp.org>

--- site.mk
+++ site.mk
@@ -7,25 +7,25 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
-	gluon-announced \
+	gluon-respondd \
 	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-autoupdater \
-	gluon-setup-mode \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
+	gluon-luci-wifi-config \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
-	gluon-luci-wifi-config \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
+	gluon-setup-mode \
 	gluon-status-page \
 	iwinfo \
 	iptables \

8bb47e44090c1dd916396234948a41e77ba34f49
JKBusse <48227459+JKBusse@users.noreply.github.com>
2025-02-05 00:38:11 +0100

    Updated Gateway Names

--- site.mk
+++ site.mk
@@ -9,7 +9,7 @@
 GLUON_MULTIDOMAIN=1
 
 #DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
-DEFAULT_GLUON_RELEASE := 0.1Alpha
+DEFAULT_GLUON_RELEASE := 0.2Alpha
 # Variables set with ?= can be overwritten from the command line
 
 ##	GLUON_RELEASE

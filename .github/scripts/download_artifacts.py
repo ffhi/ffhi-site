76bf0ed5f8d23ac26872ade0d480d0a166669f02
Maximaleffekt <binarydreamer@protonmail.com>
2025-02-24 18:39:36 +0100

    Removed hardcoded file name from python script

--- .github/scripts/download_artifacts.py
+++ .github/scripts/download_artifacts.py
@@ -3,14 +3,14 @@ import requests
 import sys
 
 urls = {}
-with open('artifacts.json') as f:
+with open(sys.argv[1]) as f:
     artifacts = json.load(f)
     for artifact in artifacts['artifacts']:
         urls[artifact['name']] = artifact['archive_download_url']
 
 headers = {
     'Accept': 'application/vnd.github+json',
-    'Authorization': 'Bearer ' + sys.argv[1],
+    'Authorization': 'Bearer ' + sys.argv[2],
     'X-GitHub-Api-Version': '2022-11-28',
 }
 
@@ -18,6 +18,5 @@ for url in urls:
     print("Downloading artifact for architecture " + url)
     response = requests.get(urls[url], headers=headers)
     with open(url + '.zip', 'wb') as r:
-        print("Writing zip for " + url)
         r.write(response.content)
-        print("File written")
+        print("Wrote zip for " + url)
